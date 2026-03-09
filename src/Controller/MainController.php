28dfc812e67c616752ee8bf83095e7e6bee97e9a
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-09 16:44:43 +0100

    ajout d'informations serveurs dans la page
    Signed-off-by: thomaslinux <182340067+thomaslinux@users.noreply.github.com>

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Constraints\Date;
 
 final class MainController extends AbstractController
 {
@@ -17,7 +18,15 @@ final class MainController extends AbstractController
     #[Route('/test', name: 'main_test')]
     public function test(): Response
     {
-        return $this->render('main/test.html.twig');
+        $serie = ['name' => 'Dragon Ball Z', 'author' => 'Toriyamaha', 'nbEpisode' => 291];
+
+        $username = '<h1>Sylvain</h1>'
+
+        return $this->render('main/test.html.twig', [
+            'mySerie' => $serie,
+            'date' => new \DateTime(),
+            'username' => $username
+        ]);
     }
-    
+
 }
