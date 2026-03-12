fe4572ac321f01abc62e73fda3f67dae0d60e7d2
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-12 10:09:36 +0100

    #[ORM\HasLifecycleCallbacks]
    Signed-off-by: thomaslinux <182340067+thomaslinux@users.noreply.github.com>

--- src/Entity/Serie.php
+++ src/Entity/Serie.php
@@ -6,6 +6,7 @@ use App\Repository\SerieRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\HasLifecycleCallbacks]
 #[ORM\Entity(repositoryClass: SerieRepository::class)]
 class Serie
 {
