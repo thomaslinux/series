81e287cf92e989b804b7a0ce0a975f5355d263f5
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-18 12:00:45 +0100

    récupération des données deserialize
    Signed-off-by: thomaslinux <182340067+thomaslinux@users.noreply.github.com>

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Constraints\Date;
 
 final class MainController extends AbstractController
@@ -16,15 +17,17 @@ final class MainController extends AbstractController
     }
 
     #[Route('/test', name: 'main_test')]
-    public function test(): Response
+    public function test(SerializerInterface $serializer): Response
     {
         $serie = ['name' => 'Dragon Ball Z', 'author' => 'Toriyamaha', 'nbEpisode' => 291];
 
         $username = '<h1>Sylvain</h1>';
 
         $joke = file_get_contents("https://api.chucknorris.io/jokes/random");
-        $joke = (json_decode($joke, true));
-        dump($joke['value']);
+//        $joke = (json_decode($joke, true));
+//        dump($joke['value']);
+
+        $serializer->deserialize($joke, \stdClass::class, 'json');
 
         return $this->render('main/test.html.twig', [
             'mySerie' => $serie,
